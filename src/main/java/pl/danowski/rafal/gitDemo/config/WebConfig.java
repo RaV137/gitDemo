2255e599d3b0d40836b38b99100a6239cfbfc470
RaV137 <rafalek96@gmail.com>
2021-03-18 17:35:14 +0100

    2 web config methods + ver 1.0.2-SNAPSHOT

---
+++ src/main/java/pl/danowski/rafal/gitDemo/config/WebConfig.java
@@ -0,0 +1,11 @@
+package pl.danowski.rafal.gitDemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+public class WebConfig extends WebSecurityConfiguration {
+    // jakieś metody conf
+
+    // jeszcze inna metoda
+}
