91ece3511cb3afa76d1644be4c6ed717a280dc14
RaV137 <rafalek96@gmail.com>
2021-03-18 16:55:55 +0100

    init commit

---
+++ src/test/java/pl/danowski/rafal/gitDemo/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.danowski.rafal.gitDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
